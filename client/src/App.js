d82c34039ee2d48d99eeaf09922d02f325e46cbd
neilkotun <neilkotun@gmail.com>
2021-04-15 17:13:05 -0400

    adding formatted navigation bar using react-bootstrap Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import Header from "./components/Header/Header"
 
 const App = () => {
   return (
-    <div>
+    <div className = "topnav">
       <Header />
       <Switch>
         <Route exact path="/Home" component={Home} />
