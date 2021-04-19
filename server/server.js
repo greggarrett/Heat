0630b4642669b99f98120bdfdd4af99b8885af04
neilkotun <neilkotun@gmail.com>
2021-04-19 16:30:51 -0400

    include googleapis in package.json

--- server/server.js
+++ server/server.js
@@ -95,12 +95,16 @@ function listEvents(auth) {
     orderBy: 'startTime',
   }, (err, res) => {
     if (err) return console.log('The API returned an error: ' + err);
-    const events = res.data.items;
+
+	// res.setHeader('Content-Type', 'application/json');
+
+	const events = res.data.items;
+
     if (events.length) {
       console.log('Upcoming 10 events:');
       events.map((event, i) => {
         const start = event.start.dateTime || event.start.date;
-        console.log(`${start} - ${event.summary}`);
+		console.log(`${start} - ${event.summary}`);
       });
     } else {
       console.log('No upcoming events found.');
