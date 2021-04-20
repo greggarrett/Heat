2351798ab8380fea395565ea691497789440a4ee
katpetruzzelli <75508297+katpetruzzelli@users.noreply.github.com>
2021-04-19 23:34:04 -0400

    contact us

--- client/src/views/Contact/Contact.js
+++ client/src/views/Contact/Contact.js
@@ -3,16 +3,241 @@
 
 import React from 'react';
 // import logo from '../../assets/club_logo.png';
-import '../Home/Home.css';
+import './Contact.css';
+import logo from '../../assets/mollykennedy.jpg';
+import logo1 from '../../assets/oliviagreen.jpg';
+import logo2 from '../../assets/liatsinclair.jpg';
+import logo3 from '../../assets/kerlinlemus.jpg';
+import logo4 from '../../assets/alexperez.jpg';
+import logo5 from '../../assets/sydneyure.jpg';
+import logo6 from '../../assets/reganhardin.jpg';
+import logo7 from '../../assets/sophiavar.jpg';
+import logo8 from '../../assets/mollymetcalf.jpg';
+import logo9 from '../../assets/lillymagley.jpg';
+import logo10 from '../../assets/briguildin.jpg';
+import logo11 from '../../assets/ashleycarillo.jpg';
+import logo12 from '../../assets/taylorjackson.jpg';
+import logo13 from '../../assets/abbiewolfe.jpg';
+// import logo14 from '../../assets/mandymusleh.png';
+import logo15 from '../../assets/camterry.jpg';
+import logo16 from '../../assets/andreacastro.jpg';
+import logo17 from '../../assets/zoezimmerman.jpg';
 
-function Home() {
+function Contact() {
     return (
         <div className="App">
             <header className="App-header">
                 {/* <img src={logo} className="App-logo" alt="logo" /> */}
                 <p>
                     {/* Edit <code>src/App.js</code> and save to reload. */}
-                    Contact us!
+
+                    <h1> Contact Us! </h1>
+        
+                    <div class="column">
+                        <div class="row-md-3 row-sm-6 row-xs-12">
+                            <img src={logo} className="App-logo" alt="logo"></img>
+                            <p> <p> ________________________________________ </p> </p>
+                            <h4>Chapter President</h4>
+                            <p>Molly Kennedy</p>
+                            <p> <span class="contactinfo">850-509-7087</span> </p>
+                            <p> <span class="contactinfo">ufalphaomicronpipresident@gmail.com</span> </p>
+                        </div>
+                    </div>
+
+                    <div class="column">
+                        <div class="row-md-3 row-sm-6 row-xs-12">
+                            <img src={logo1} className="App-logo" alt="logo"></img>
+                            <p> <p> ________________________________________ </p> </p>
+                            <h4>Vice President</h4>
+                            <p>Olivia Green</p>
+                            <p> <span class="contactinfo">770-713-7518</span> </p>
+                            <p> <span class="contactinfo">aoiiufvpa@gmail.com</span> </p>
+                        </div>
+                    </div>
+
+                    <div class="column">
+                        <div class="row-md-3 row-sm-6 row-xs-12">
+                            <img src={logo2} className="App-logo" alt="logo"></img>
+                            <p> <p> ________________________________________ </p> </p>
+                            <h4>VP of Standards</h4>
+                            <p>Liat Sinclair</p>
+                            <p> <span class="contactinfo">305-804-1888</span> </p>
+                            <p> <span class="contactinfo">ufaoiistandards@gmail.com</span> </p>
+                        </div>
+                    </div>
+
+                    <div class="column">
+                        <div class="row-md-3 row-sm-6 row-xs-12">
+                            <img src={logo3} className="App-logo" alt="logo"></img>
+                            <p> <p> ________________________________________ </p> </p>
+                            <h4>Sisterhood Chair</h4>
+                            <p>Kerlin Lemus</p>
+                            <p> <span class="contactinfo">786-702-7407</span> </p>
+                            <p> <span class="contactinfo">ufaoiisisterhood@gmail.com</span> </p>
+                        </div>
+                    </div>
+                    <p> <span class="spacing">.</span> </p>
+
+                    <div class="column">
+                        <div class="row-md-3 row-sm-6 row-xs-12">
+                            <img src={logo4} className="App-logo" alt="logo"></img>
+                            <p> <p> ________________________________________ </p> </p>
+                            <h4>VP of Academics</h4>
+                            <p>Alex Perez</p>
+                            <p> <span class="contactinfo">305-989-6823</span> </p>
+                            <p> <span class="contactinfo">ufaoiiacademics@gmail.com</span> </p>
+                        </div>
+                    </div>
+
+                    <div class="column">
+                        <div class="row-md-3 row-sm-6 row-xs-12">
+                            <img src={logo5} className="App-logo" alt="logo"></img>
+                            <p> <p> ________________________________________ </p> </p>
+                            <h4>VP of Finance </h4>
+                            <p>Sydney Ure</p>
+                            <p> <span class="contactinfo">954-798-8966</span> </p>
+                            <p> <span class="contactinfo">ufaoiitreasurer@gmail.com</span> </p>
+                        </div>
+                    </div>
+
+                    <div class="column">
+                        <div class="row-md-3 row-sm-6 row-xs-12">
+                            <img src={logo6} className="App-logo" alt="logo"></img>
+                            <p> <p> ________________________________________ </p> </p>
+                            <h4>Chapter Secretary </h4>
+                            <p>Regan Hardin</p>
+                            <p> <span class="contactinfo">813-507-0659</span> </p>
+                            <p> <span class="contactinfo">ufaoiisecretary@gmail.com</span> </p>
+                        </div>
+                    </div>
+
+                    <div class="column">
+                        <div class="row-md-3 row-sm-6 row-xs-12">
+                            <img src={logo7} className="App-logo" alt="logo"></img>
+                            <p> <p> ________________________________________ </p> </p>
+                            <h4>New Member Educator</h4>
+                            <p>Sophia Varjabedian</p>
+                            <p> <span class="contactinfo">813-760-7646</span> </p>
+                            <p> <span class="contactinfo">ufaoiinme@gmail.com</span> </p>
+                        </div>
+                    </div>
+                    <p> <span class="spacing">.</span> </p>
+
+                    <div class="column">
+                        <div class="row-md-3 row-sm-6 row-xs-12">
+                            <img src={logo8} className="App-logo" alt="logo"></img>
+                            <p> <p> ________________________________________ </p> </p>
+                            <h4>Panhellenic Delegate </h4>
+                            <p>Molly Metcalf</p>
+                            <p> <span class="contactinfo">772-696-2882</span> </p>
+                            <p> <span class="contactinfo">ufaoiipanhellenicdelegate@gmail.com</span> </p>
+                        </div>
+                    </div>
+
+                    <div class="column">
+                        <div class="row-md-3 row-sm-6 row-xs-12">
+                            <img src={logo9} className="App-logo" alt="logo"></img>
+                            <p> <p> ________________________________________ </p> </p>
+                            <h4>Keeper of the Ritual</h4>
+                            <p>Lilly Magley</p>
+                            <p> <span class="contactinfo">904-210-6487</span> </p>
+                            <p> <span class="contactinfo">ufaoiiritual@gmail.com</span> </p>
+                        </div>
+                    </div>
+
+                    <div class="column">
+                        <div class="row-md-3 row-sm-6 row-xs-12">
+                            <img src={logo10} className="App-logo" alt="logo"></img>
+                            <p> <p> ________________________________________ </p> </p>
+                            <h4>VP of Communications</h4>
+                            <p>Bri Guildin</p>
+                            <p> <span class="contactinfo">407-808-6259</span> </p>
+                            <p> <span class="contactinfo">ufaoiicommunications@gmail.com</span> </p>
+                        </div>
+                    </div>
+
+                    <div class="column">
+                        <div class="row-md-3 row-sm-6 row-xs-12">
+                            <img src={logo11} className="App-logo" alt="logo"></img>
+                            <p> <p> ________________________________________ </p> </p>
+                            <h4> External Philanthropy</h4>
+                            <p>Ashley Carillo</p>
+                            <p> <span class="contactinfo">954-296-9368</span> </p>
+                            <p> <span class="contactinfo">ufaoiiphilanthropy@gmail.com</span> </p>
+                        </div>
+                    </div>
+                    <p> <span class="spacing">.</span> </p>
+
+                    <div class="column">
+                        <div class="row-md-3 row-sm-6 row-xs-12">
+                            <img src={logo12} className="App-logo" alt="logo"></img>
+                            <p> <p> ________________________________________ </p> </p>
+                            <h4>Social Chairman</h4>
+                            <p>Taylor Jackson</p>
+                            <p> <span class="contactinfo">863-617-4597</span> </p>
+                            <p> <span class="contactinfo">ufaoiisocialchair@gmail.com</span> </p>
+                        </div>
+                    </div>
+
+                    <div class="column">
+                        <div class="row-md-3 row-sm-6 row-xs-12">
+                            <img src={logo13} className="App-logo" alt="logo"></img>
+                            <p> <p> ________________________________________ </p> </p>
+                            <h4>Alumnae Relations</h4>
+                            <p>Abbie Wolfe</p>
+                            <p> <span class="contactinfo">561-713-5672</span> </p>
+                            <p> <span class="contactinfo">aopialum@gmail.com</span> </p>
+                        </div>
+                    </div>
+
+                    {/* <div class ="column">
+                    <div class= "row-md-3 row-sm-6 row-xs-12">
+                    <img src={logo14} className="App-logo" alt="logo"></img>
+                         <p> <p> ________________________________________ </p> </p>
+                        <h4>Vice President of Membership Recruitment</h4>
+                            <p>Mandy Musleh</p>
+                                <p>
+                                224-688-5598
+                                </p>
+                    </div>
+                </div> */}
+
+                    <div class="column">
+                        <div class="row-md-3 row-sm-6 row-xs-12">
+                            <img src={logo15} className="App-logo" alt="logo"></img>
+                            <p> <p> ________________________________________ </p> </p>
+                            <h4>House Manager</h4>
+                            <p>Cam Terry</p>
+                            <p> <span class="contactinfo">904-625-8293</span> </p>
+                            <p> <span class="contactinfo">ufaoiihousemanager@gmail.com</span> </p>
+                        </div>
+                    </div>
+
+                    <div class="column">
+                        <div class="row-md-3 row-sm-6 row-xs-12">
+                            <img src={logo16} className="App-logo" alt="logo"></img>
+                            <p> <p> ________________________________________ </p> </p>
+                            <h4>Internal Philanthropy</h4>
+                            <p>Andrea Castro</p>
+                            <p> <span class="contactinfo">786-368-0503</span> </p>
+                            <p> <span class="contactinfo">ufstrikeoutarthritis@gmail.com</span> </p>
+                        </div>
+                    </div>
+
+                    <p> <span class="spacing">.</span> </p>
+
+                    <div class="column">
+                        <div class="row-md-3 row-sm-6 row-xs-12">
+                            <img src={logo17} className="App-logo" alt="logo"></img>
+                            <p> <p> ________________________________________ </p> </p>
+                            <h4>VP of Education</h4>
+                            <p>Zoe Zimmerman</p>
+                            <p> <span class="contactinfo">496-766-2536</span> </p>
+                            <p> <span class="contactinfo">ufaoiieducation@gmail.com</span> </p>
+                        </div>
+                    </div>
+
+
                 </p>
                 <a
                     className="App-link"
@@ -26,4 +251,4 @@ function Home() {
     );
 }
 
-export default Home;
+export default Contact;
